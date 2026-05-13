a88ffe3f7291d9b585c8afd868e403c5f9195294
Godswill Akaba <godswillakaba@gmail.com>
2026-05-13 16:54:08 +0000

    update

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -3,4 +3,7 @@ const navMenu = document.getElementById('nav-menu');
 
 navButton.addEventListener("click", () => {
     navMenu.classList.toggle("open");
+    navButton.classList.toggle("open");
 });
+
+
