d511f0a7414aa2559c9d33c53616e3db839a2fcc
Godswill Akaba <godswillakaba@gmail.com>
2026-05-28 16:08:07 +0000

    update

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -5,7 +5,7 @@ const directory = document.querySelector("#directory");
 
 
 
-const url = "data/members.json";
+const url = "/wdd231/data/members.json";
 
 async function getMembers() {
     const response = await fetch(url);
